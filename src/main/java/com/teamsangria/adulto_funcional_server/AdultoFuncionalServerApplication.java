5939e7abeafb3a8ffbfb073742e21350e867a73a
MIGUELBLANDON <blandonmontesmiguelangel@gmail.com>
2026-03-04 14:58:07 -0500

    proyecto recien creado

---
+++ src/main/java/com/teamsangria/adulto_funcional_server/AdultoFuncionalServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamsangria.adulto_funcional_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultoFuncionalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdultoFuncionalServerApplication.class, args);
+	}
+
+}
