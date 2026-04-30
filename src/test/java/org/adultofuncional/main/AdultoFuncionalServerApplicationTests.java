57ee05f96b967a4500574b5a304901d4ec12d498
Jeanzo1719 <sebasrios1119@gmail.com>
2026-04-30 16:34:13 -0500

    chore: test eliminados por innecesidad

--- src/test/java/org/adultofuncional/main/AdultoFuncionalServerApplicationTests.java
+++ src/test/java/org/adultofuncional/main/AdultoFuncionalServerApplicationTests.java
@@ -1,6 +1,6 @@
 package org.adultofuncional.main;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
