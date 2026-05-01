8df6bdd89a84d7da5d09787731d6d4428c8b6efe
Sudo-CrocPenguin <blandonmontesmiguelangel@gmail.com>
2026-05-01 18:30:43 -0500

    feat: implementacion de PasswordResponse

--- src/main/java/org/adultofuncional/main/security/application/dto/PasswordResponse.java
+++ src/main/java/org/adultofuncional/main/security/application/dto/PasswordResponse.java
@@ -1,5 +1,24 @@
 package org.adultofuncional.main.security.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordResponse {
-    
+
+    private UUID id;
+
+    private String applicationName;
+
+    private String password;
+
+    private LocalDate lastChangeDate;
 }
