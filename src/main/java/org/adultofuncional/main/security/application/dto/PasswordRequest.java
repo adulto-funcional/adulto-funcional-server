2f3b6471c95a2d56c96ffd7be450586157213d4b
Sudo-CrocPenguin <blandonmontesmiguelangel@gmail.com>
2026-05-01 18:28:00 -0500

    feat: implementacion de PasswordRequest

--- src/main/java/org/adultofuncional/main/security/application/dto/PasswordRequest.java
+++ src/main/java/org/adultofuncional/main/security/application/dto/PasswordRequest.java
@@ -1,5 +1,25 @@
 package org.adultofuncional.main.security.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordRequest {
-    
+
+    @NotBlank(message = "El nombre de la aplicación es obligatorio")
+    @Size(max = 35, message = "El nombre de la aplicación no puede exceder 35 caracteres")
+    private String applicationName;
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 1, message = "La contraseña no puede estar vacía")
+    private String password;
+    private LocalDate lastChangeDate;
 }
