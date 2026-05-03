14c1dd7af0d4dc190dcd765a8afd46bbe666b31b
Jeanzo1719 <sebasrios1119@gmail.com>
2026-05-03 17:12:46 -0500

    fix: variables de entorno ausentes en el docker-compose.yml añadidas y header ausente en SecurityConfig añadido

--- src/main/java/org/adultofuncional/main/config/security/SecurityConfig.java
+++ src/main/java/org/adultofuncional/main/config/security/SecurityConfig.java
@@ -127,7 +127,7 @@ public class SecurityConfig {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowedOrigins(allowedOrigins);
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    config.setAllowedHeaders(List.of("Content-Type", "X-Requested-With"));
+    config.setAllowedHeaders(List.of("Content-Type", "X-Requested-With", "X-Client-Type"));
     config.setExposedHeaders(List.of());
     config.setAllowCredentials(true);
     config.setMaxAge(3600L);
