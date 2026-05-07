eaadc39d1746012678e40c5b81f08e93742bbe54
lidysjaraba <lidys.jarabagu@amigo.edu.co>
2026-05-06 19:44:16 -0500

    feat: se implementa repositorio de movimientos en capa de infraestructura

---
+++ src/main/java/org/adultofuncional/main/finances/infrastructure/repository/MovementRepositoryImpl.java
@@ -0,0 +1,50 @@
+package org.adultofuncional.main.finances.infrastructure.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.adultofuncional.main.finances.domain.model.Movement;
+import org.adultofuncional.main.finances.domain.repository.MovementRepository;
+import org.adultofuncional.main.finances.infrastructure.persistence.entity.MovementEntity;
+import org.adultofuncional.main.finances.infrastructure.persistence.mapper.MovementMapper;
+import org.adultofuncional.main.finances.infrastructure.persistence.repository.SpringMovementJpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Repository
+@RequiredArgsConstructor
+public class MovementRepositoryImpl implements MovementRepository {
+    
+    private final SpringMovementJpaRepository jpaRepository;
+    private final MovementMapper mapper;
+
+    @Override
+    public Optional<Movement> findById(UUID id) {
+        return jpaRepository.findById(id).map(mapper::toDomain);
+    }
+
+
+    @Override
+    public List<Movement> findAllByAccountId(UUID accountId) {
+        return jpaRepository.findAllByAccount_AccountId(accountId)
+        .stream().map(mapper::toDomain).toList();
+    }
+
+
+    @Override
+    public Movement save(Movement movement) {
+
+        MovementEntity entity = mapper.toEntity(movement, movement.getId());
+        MovementEntity saved = jpaRepository.save(entity);
+
+        return mapper.toDomain(saved);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        jpaRepository.deleteById(id);
+    }
+}
