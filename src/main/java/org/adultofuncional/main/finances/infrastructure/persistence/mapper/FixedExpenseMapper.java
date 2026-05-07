de2df358bf877fe16325c3795fd8e2ab46e0b6ad
lidysjaraba <lidys.jarabagu@amigo.edu.co>
2026-05-06 19:43:13 -0500

    feat: se agrega estructura base del mapper de gastos fijos

---
+++ src/main/java/org/adultofuncional/main/finances/infrastructure/persistence/mapper/FixedExpenseMapper.java
@@ -0,0 +1,12 @@
+package org.adultofuncional.main.finances.infrastructure.persistence.mapper;
+
+import java.util.UUID;
+
+import org.adultofuncional.main.finances.domain.model.FixedExpense;
+import org.adultofuncional.main.finances.infrastructure.persistence.entity.FixedExpensesEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FixedExpenseMapper {
+    
+}
